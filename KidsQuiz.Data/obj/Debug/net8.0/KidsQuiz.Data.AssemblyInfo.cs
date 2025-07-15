34e54d1589ddbcfd7b79f4b653683bdbfc4f327a
Fei Li <feili0607@gmail.com>
2025-07-15 20:57:50 +1000

    set up the openAI API key ,but need to update the key detail

--- KidsQuiz.Data/obj/Debug/net8.0/KidsQuiz.Data.AssemblyInfo.cs
+++ KidsQuiz.Data/obj/Debug/net8.0/KidsQuiz.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KidsQuiz.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c063ef07cf27dc45594db24ccf20b99fffe367d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+694f3fe6bd06571542569860603a83a9af95b826")]
 [assembly: System.Reflection.AssemblyProductAttribute("KidsQuiz.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KidsQuiz.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- KidsQuiz.Services/obj/Debug/net9.0/KidsQuiz.Services.AssemblyInfo.cs
+++ KidsQuiz.Services/obj/Debug/net9.0/KidsQuiz.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KidsQuiz.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c063ef07cf27dc45594db24ccf20b99fffe367d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+694f3fe6bd06571542569860603a83a9af95b826")]
 [assembly: System.Reflection.AssemblyProductAttribute("KidsQuiz.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KidsQuiz.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
